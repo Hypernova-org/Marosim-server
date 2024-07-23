8c3733bb6b4992ddeaf31e9f094efe0e8c415142
Akbar Odilov <akbarjonodilov01@gmail.com>
2024-07-23 17:32:55 +0500

    17:32 | 07/23/2024 | feat: Add product routes to router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const userRoutes = require("./user.routes.js");
+const productRoutes = require("./product.routes.js");
 const categoryRoutes = require("./category.routes.js");
 const translationRoutes = require("./translation.routes.js");
 const router = Router();
 
 router.use("/users", userRoutes);
+router.use("/products", productRoutes);
 router.use("/categories", categoryRoutes);
 router.use("/translations", translationRoutes);
 
