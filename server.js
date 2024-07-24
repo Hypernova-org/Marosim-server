6e0e578b71829c8755d646497310f4bc51cadcc4
Akbar Odilov <akbarjonodilov01@gmail.com>
2024-07-24 10:43:57 +0500

    chore: Fix typo in server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use("/api", routes);
 app.get("/", (req, res) => {
   return res.json({ message: "Server is run!" });
-});
+}); 
 
 function startServerOnPort(port) {
   const listen = app.listen(port, () =>
